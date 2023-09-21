b366d353b26cf8042d79453eafd421de132b5035
= <=>
2023-09-21 15:00:16 +0300

    m

--- apps/main/src/app.module.ts
+++ apps/main/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { RmqModule } from '../../../rmq/rmq.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
-  imports: [RmqModule.register({ name: 'TEST' })],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'TEST',
+        transport: Transport.RMQ,
+        options: {
+          urls: [
+            'amqps://gkxlkdbz:19Bj2b2918YxXR8qq3T93lXIW6e89pg5@sparrow.rmq.cloudamqp.com/gkxlkdbz',
+          ],
+          queue: 'TEST',
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
 })
 export class AppModule {}

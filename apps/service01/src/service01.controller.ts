bc71f91b5c12522eb6dbe0032811e346bf48131f
= <=>
2023-09-21 14:36:52 +0300

    fix

--- apps/service01/src/service01.controller.ts
+++ apps/service01/src/service01.controller.ts
@@ -1,10 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class Service01Controller {
+  private logger: Logger = new Logger(Service01Controller.name);
+
   @EventPattern('pattern')
   public async testPattern(data: string): Promise<void> {
     console.log(data);
+    this.logger.log('ok works fine');
   }
 }
--- apps/service01/src/service01.module.ts
+++ apps/service01/src/service01.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { Service01Controller } from './service01.controller';
-import { RmqModule } from 'rmq/rmq.module';
+import { RmqModule } from '../../../rmq/rmq.module';
 
 @Module({
   imports: [RmqModule],

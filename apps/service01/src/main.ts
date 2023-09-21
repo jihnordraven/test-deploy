c648447c2249aec352173389bb825045e5410abe
= <=>
2023-09-21 14:44:22 +0300

    fix

--- apps/service01/src/main.ts
+++ apps/service01/src/main.ts
@@ -4,23 +4,6 @@ import { RmqService } from '../../../rmq/rmq.service';
 import { Service01Module } from './service01.module';
 
 const bootstrap = async () => {
-  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-  //   FilesModule,
-  //   {
-  //     transport: Transport.RMQ,
-  //     options: {
-  //       urls: ['amqp://localhost:5672'],
-  //       queue: 'MAIN',
-  //       noAck: true,
-  //       persistent: true,
-  //     },
-  //   },
-  // );
-
-  // await app.listen();
-
-  // Exactly the same ---
-
   const app: INestApplication =
     await NestFactory.create<INestApplication>(Service01Module);
 
--- apps/service01/src/service01.module.ts
+++ apps/service01/src/service01.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { Service01Controller } from './service01.controller';
 import { RmqModule } from '../../../rmq/rmq.module';
+import { RmqService } from 'rmq/rmq.service';
 
 @Module({
   imports: [RmqModule],
   controllers: [Service01Controller],
+  providers: [RmqService],
 })
 export class Service01Module {}
